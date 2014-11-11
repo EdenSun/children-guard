4afc299bd87b9b1f8dfab7df3d39f747670af3dd
eden.sun <durian.suen@gmail.com>
2014-11-11 15:41:49 +0800

    child end app password page

--- app/client-end/src/eden/sun/childrenguard/child/activity/AppPasswordActivity.java
+++ app/client-end/src/eden/sun/childrenguard/child/activity/AppPasswordActivity.java
@@ -7,21 +7,26 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import eden.sun.childrenguard.child.R;
+import eden.sun.childrenguard.child.db.dao.ChildSettingDao;
 
 public class AppPasswordActivity extends Activity {
     private EditText lockPasswordEditText;  
-    private Button okBtn;
+    private Button unlockBtn;
     private Button cancelBtn;
     private String password;  
-    
+    private ChildSettingDao childSettingDao;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_app_password);
+		childSettingDao = new ChildSettingDao(this);
 		
 		initComponent();
 		
-		okBtn.setOnClickListener(new OnClickListener(){
+		// get password 
+		password = childSettingDao.getAppLockPassword();
+				
+		unlockBtn.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View arg0) {
@@ -44,7 +49,7 @@ public class AppPasswordActivity extends Activity {
 
 	private void initComponent() {
 		lockPasswordEditText = (EditText)findViewById(R.id.lockPasswordEditText);
-		okBtn = (Button)findViewById(R.id.okBtn);
+		unlockBtn = (Button)findViewById(R.id.unlockBtn);
 		cancelBtn = (Button)findViewById(R.id.cancelBtn);
 	}
 
