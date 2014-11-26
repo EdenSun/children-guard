505684a9ef53abe56ebc1e5906ea8b8a50c58404
DurianSuen <durian.suen@gmail.com>
2014-11-27 06:43:32 +0800

    remove useless code

--- app/parent-end/ChildrenGuard/src/eden/sun/childrenguard/activity/PasswordResetActivity.java
+++ app/parent-end/ChildrenGuard/src/eden/sun/childrenguard/activity/PasswordResetActivity.java
@@ -192,25 +192,6 @@ public class PasswordResetActivity extends CommonActivity {
 		return true;
 	}
 	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.password_reset, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
 	/*@Override
 	protected void onStart() {
 		super.onStart();
