409ab5631a12c57cdac79103124df9cf5b0d47eb
eden.sun <durian.suen@gmail.com>
2015-06-08 15:06:27 +0800

    Client端服务IP修改为域名

--- app/client-end/src/eden/sun/childrenguard/child/util/Config.java
+++ app/client-end/src/eden/sun/childrenguard/child/util/Config.java
@@ -1,7 +1,8 @@
 package eden.sun.childrenguard.child.util;
 
 public class Config {
-	public static final String BASE_URL = "http://192.168.2.101:8080/childrenguard-server/";
+	public static final String BASE_URL = "http://www.soloappinfo.com:8080/childrenguard-server/";
+//	public static final String BASE_URL = "http://169.7.231.67:8080/childrenguard-server/";
 	public static final String BASE_URL_MVC = BASE_URL + "mvc/";
 	public static final String BASE_URL_COMETD =  BASE_URL +  "cometd/";
 		
